efcf520c87fe180942469dce39780891c8309afd
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-01-25 17:43:23 -0500

    Disable demo events

--- inngest/createDemoEvents.ts
+++ inngest/createDemoEvents.ts
@@ -7,34 +7,34 @@ import casual from "casual";
 
 export default inngest.createScheduledFunction(
   "Create Demo Events",
-  "* * * * *",
+  "0 * * * *",
   async () => {
-    const events = [
-      {
-        name: "app/landing_page.created",
-        data: {
-          page_id: casual.uuid,
-          page_title: casual.company_name,
-          url: casual.url,
-        },
-      },
-      {
-        name: "app/landing_page.created",
-        data: {
-          page_id: casual.uuid,
-          page_title: casual.company_name,
-          url: casual.url,
-        },
-      },
-      {
-        name: "app/user.signup",
-        data: {
-          user_id: casual.uuid,
-          email: casual.email,
-          name: casual.name,
-        },
-      },
-    ];
-    await inngest.send(events);
+    // const events = [
+    //   {
+    //     name: "app/landing_page.created",
+    //     data: {
+    //       page_id: casual.uuid,
+    //       page_title: casual.company_name,
+    //       url: casual.url,
+    //     },
+    //   },
+    //   {
+    //     name: "app/landing_page.created",
+    //     data: {
+    //       page_id: casual.uuid,
+    //       page_title: casual.company_name,
+    //       url: casual.url,
+    //     },
+    //   },
+    //   {
+    //     name: "app/user.signup",
+    //     data: {
+    //       user_id: casual.uuid,
+    //       email: casual.email,
+    //       name: casual.name,
+    //     },
+    //   },
+    // ];
+    // await inngest.send(events);
   }
 );
